852ff9f0182aca4daeeb6856f9b08af4e6fea0f1
Shuchita Khare <shuchitak@xmos.com>
2022-05-20 10:19:23 +0100

    Hardcode version as workaround for version mismatch error

--- lib_tflite_micro/api/version.h
+++ lib_tflite_micro/api/version.h
@@ -5,7 +5,7 @@
 namespace lib_tflite_micro {
 
 unsigned major_version = 0;
-unsigned minor_version = 1;
+unsigned minor_version = 0; // TODO Workaround till https://xmosjira.atlassian.net/browse/XT-205 is fixed
 unsigned patch_version = 0;
 
 } // namespace lib_tflite_micro
